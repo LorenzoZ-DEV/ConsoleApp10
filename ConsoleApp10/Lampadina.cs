ec3e4db85985d4964bbe3931f573b99fddbd7ff1
Lore <Lore@DESKTOP-J005ERJ>
2024-08-19 16:33:40 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp10/Lampadina.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Lampadina
+    {
+    public int PotenzaInWatt { get; set; } = 0;
+        public int Durata {  get; set; }
+        public string Statolampadina { get; set; } = string.Empty;
+        public int nled;
+        public string indirizzoIP;
+        public string NomeLampadina;
+         public string NumClienti;
+        public Lampadina(int potenzainwatt, int durata)
+        {
+            PotenzaInWatt = potenzainwatt;
+            Durata = durata;    
+        }
+
+    public Lampadina()
+    {
+        indirizzoIP = string.Empty;
+        nled = 0;
+        NomeLampadina = string.Empty;
+        NumClienti = string.Empty;
+    }
+
+    public void Accendi(string statolampadina)
+        {
+        Statolampadina = statolampadina;
+
+        }
+         public void MostraLampadina()
+    {
+        Console.WriteLine("Lampadina:" + Statolampadina);
+    }
+        public void Spegni()
+        {
+           Console.WriteLine("Lampadina" + Statolampadina +"all'indirizzo" + indirizzoIP);
+        }
+    }
+
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Lampadina lampadina = new Lampadina();
+
+        lampadina.nled = 50;
+        lampadina.indirizzoIP = "127.168.102.3";
+        lampadina.Statolampadina = "Accesa";
+        lampadina.NomeLampadina = "Philips Hue";
+        lampadina.PotenzaInWatt = 15;
+
+        Console.WriteLine("Nome:" + lampadina.NomeLampadina);
+        Console.WriteLine("Indirizzo IP" + lampadina.indirizzoIP);
+        Console.WriteLine("Stato" + lampadina.Statolampadina);
+        Console.WriteLine("Potenza" + lampadina.PotenzaInWatt + "W");
+    }
+}
